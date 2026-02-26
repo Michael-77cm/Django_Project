16f234118058e85e005270c4856333d85e53547c
Michael-77cm <michaelbello175@gmail.com>
2026-02-26 06:06:14 +0000

    added view to views.py file in hello world app

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.urls import HttpResponse
 # Create your views here.
+
+def home_page_view(request):
+	return HttpResponse("<h1>Hello, World!</h1>")
